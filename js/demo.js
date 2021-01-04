8ec88a2ccc32a2d8d1b8f218af1d0009475e5779
tingyu6106 <76472780+tingyu6106@users.noreply.github.com>
2021-01-04 19:39:49 +0800

    87878787878787

--- js/demo.js
+++ js/demo.js
@@ -1,4 +1,9 @@
 var self = document.getElementById("self")
 self.onclick = function() {
- document.getElementById("selfintro").style.display = "block";
-} 
+    document.getElementById("selfintro").style.display = "block";
+}
+
+var introclose = document.getElementById("introclose")
+introclose.onclick = function() {
+    document.getElementById("selfintro").style.display = "none";
+}
